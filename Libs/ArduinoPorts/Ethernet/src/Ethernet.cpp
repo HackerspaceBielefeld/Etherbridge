9570c51a296f2229d50f2f7e67834ed3e1916ab6
Florian Kiemes <florian@fa-kiemes.de>
2025-07-20 23:35:52 +0200

    Added Wiznet ioLibrary_Driver. Removed StreamLib.

--- Libs/ArduinoPorts/Ethernet/src/Ethernet.cpp
+++ Libs/ArduinoPorts/Ethernet/src/Ethernet.cpp
@@ -222,6 +222,7 @@ void EthernetClass::setRetransmissionTimeout(uint16_t milliseconds)
 void EthernetClass::setRetransmissionCount(uint8_t num)
 {
     while(wzChannel.begin() != SUCCESS);
+
 	W5100.setRetransmissionCount(num);
 	while(wzChannel.end() != SUCCESS);
 }
